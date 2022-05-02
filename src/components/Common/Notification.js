3f4a320991a01a5e5b5348c010a0efeb06beebcf
NikolayAngelov96 <103751145+NikolayAngelov96@users.noreply.github.com>
2022-05-03 00:13:58 +0300

    fixed toast position

--- src/components/Common/Notification.js
+++ src/components/Common/Notification.js
@@ -1,4 +1,5 @@
 import Toast from 'react-bootstrap/Toast';
+import './Notification.css';
 
 import { NotificationContext } from '../../contexts/NotificationContext';
 import { useContext } from 'react';
@@ -11,16 +12,15 @@ const Notification = () => {
     }
 
     return (
-        <Toast className="d-inline-block m-1" bg={notification.type} onClose={hideNotification}>
-            <Toast.Header>
-                <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-                <strong className="me-auto">{notification.type === 'danger' ? 'Oops!' : notification.type.toUpperCase()}</strong>
-            </Toast.Header>
-            <Toast.Body className='text-white'>
-                {notification.message}
-            </Toast.Body>
-        </Toast>
-        
+            <Toast className="notification d-inline-block m-1" bg={notification.type} onClose={hideNotification}>
+                <Toast.Header>
+                    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                    <strong className="me-auto">{notification.type === 'danger' ? 'Oops!' : notification.type.toUpperCase()}</strong>
+                </Toast.Header>
+                <Toast.Body className='text-white'>
+                    {notification.message}
+                </Toast.Body>
+            </Toast>
     )
 }
 
